e9b490ea31e149feb23ea90cd19b96db0194609b
Amir Hossain <tailheadeusage@gmail.com>
2024-12-08 12:47:54 +0330

    added a size and count var to make changes easier

--- js/app.js
+++ js/app.js
@@ -1,16 +1,18 @@
 const canvas = document.getElementById("main");
 const c = canvas.getContext("2d");
 
+const size_and_count = [1000, 600, 10, 5];
+
 const resize = () => {
-    canvas.width = 1000;
-    canvas.height = 600;
+    canvas.width = size_and_count[0];
+    canvas.height = size_and_count[1];
 }; resize();
 
-init_vector_space(10, 5, 1000, 600);
+init_vector_space(size_and_count[2], size_and_count[3], size_and_count[0], size_and_count[1]);
 
 function animation() {
     c.fillStyle = "black";
-    c.rect(0,0,1000,600);
+    c.rect(0,0,size_and_count[0],size_and_count[1]);
     c.fill();
 
     requestAnimationFrame(animation);
